6939d1121c8a8c6450362e700284405c1fff4105
Sebastian Pazirai <spazirai@kth.se>
2020-02-19 02:11:19 +0100

    first

---
+++ Test1.java
@@ -0,0 +1,15 @@
+public class Test1{
+
+    public static void main(String[] args){
+        System.out.println("hej");
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ start.bat
@@ -0,0 +1,4 @@
+java -Xmx4096M -Xms4096M -jar minecraft_server.1.14.4.jar 
+javac Test1.java
+java Test1
+PAUSE
